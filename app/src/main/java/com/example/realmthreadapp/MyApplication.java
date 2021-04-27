d2fe53a3827eb7d73c9dae0b9499fec3fddddc6e
savelgmx <savel@gmx.com>
2021-04-27 14:49:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/realmthreadapp/MyApplication.java
@@ -0,0 +1,23 @@
+package com.example.realmthreadapp;
+
+
+import android.app.Application;
+
+//попытка разобраться с куфдь еркувы
+//перенос приложения из https://github.com/realm/realm-java/tree/master/examples/threadExample/src/main/java/io/realm/examples/threads
+
+
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Configure Realm for the application
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.deleteRealm(realmConfiguration); // Clean slate
+        Realm.setDefaultConfiguration(realmConfiguration); // Make this Realm the default
+    }
+}
