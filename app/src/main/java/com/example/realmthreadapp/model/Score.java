cf934800a4adbc1225a6c8a1e56503f608a14a01
savelgmx <savel@gmx.com>
2021-04-28 13:08:17 +0700

    some errors still remains Re-upload still not complete

---
+++ app/src/main/java/com/example/realmthreadapp/model/Score.java
@@ -0,0 +1,25 @@
+package com.example.realmthreadapp.model;
+
+import io.realm.RealmObject;
+
+public class Score extends RealmObject {
+
+    private String name;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
